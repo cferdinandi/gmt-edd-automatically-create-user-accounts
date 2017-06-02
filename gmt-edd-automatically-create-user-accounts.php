c5cfa2f71a8373e10872e5144dd7542e03d717f6
cferdinandi <chris@gomakethings.com>
2017-06-01 20:28:48 -0400

    Removed metabox

--- gmt-edd-automatically-create-user-accounts.php
+++ gmt-edd-automatically-create-user-accounts.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/cferdinandi/gmt-edd-automatically-create-user-accounts/
  * GitHub Plugin URI: https://github.com/cferdinandi/gmt-edd-automatically-create-user-accounts/
  * Description: Automatically create a user account when someone buys a product with Easy Digital Downloads.
- * Version: 2.0.0
+ * Version: 2.1.0
  * Author: Chris Ferdinandi
  * Author URI: http://gomakethings.com
  * Text Domain: gmt_edd
@@ -13,10 +13,10 @@
  */
 
 // Define constants
-define( 'GMT_EDD_CREATE_USER_VERSION', '2.0.0' );
+define( 'GMT_EDD_CREATE_USER_VERSION', '2.1.0' );
 
 // Load plugin files
-require_once( plugin_dir_path( __FILE__ ) . 'includes/metabox.php' );
+// require_once( plugin_dir_path( __FILE__ ) . 'includes/metabox.php' );
 require_once( plugin_dir_path( __FILE__ ) . 'includes/users.php' );
 require_once( plugin_dir_path( __FILE__ ) . 'includes/settings.php' );
 
